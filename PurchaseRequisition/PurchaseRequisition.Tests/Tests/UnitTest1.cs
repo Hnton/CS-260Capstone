ccb723f40155a0aaee5ac009572672144d5e708f
Brian Belcher <43049467+brianbelcher@users.noreply.github.com>
2019-03-07 10:58:19 -0500

    some test?

--- PurchaseRequisition/PurchaseRequisition.Tests/Tests/UnitTest1.cs
+++ PurchaseRequisition/PurchaseRequisition.Tests/Tests/UnitTest1.cs
@@ -1,17 +1,28 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PurchaseRequisition;
+﻿//using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using PurchaseRequisition;
+using Xunit;
 
-namespace PurchaseRequisition.Tests.Tests
-{
-    [TestClass]
+namespace PurchaseRequisition.Tests
+{    
     public class UnitTest1
     {
-        [TestMethod]
+        /*[Fact]
         public void TestMethod1()
         {
             Assert.Equals(4, 0 + 4);
 
+        }*/
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
