53681143a7f077473416a6ef0855378225db8429
Brian Belcher <43049467+brianbelcher@users.noreply.github.com>
2019-03-07 14:39:12 -0500

    updated stub tests

--- PurchaseRequisition/XUnitTestProject1/UnitTest1.cs
+++ PurchaseRequisition/XUnitTestProject1/UnitTest1.cs
@@ -10,5 +10,23 @@ namespace XUnitTestProject1
         {
             Assert.True(true); 
         }
+
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.Equal(4, 0 + 4);
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            Console.WriteLine("In Passing Test");
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
     }
 }
