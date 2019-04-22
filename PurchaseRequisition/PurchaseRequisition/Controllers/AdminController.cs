1f3623913862e3d022d0671e11de38df5f8e89cb
Hnton <mhinton4@wvup.edu>
2019-04-22 12:05:03 -0400

    pushhhhhh

--- PurchaseRequisition/PurchaseRequisition/Controllers/AdminController.cs
+++ PurchaseRequisition/PurchaseRequisition/Controllers/AdminController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace PurchaseRequisition.Controllers
 {
+    [Authorize(Roles ="Admin")]
     public class AdminController : Controller
     {
         // GET: Admin
--- PurchaseRequisition/PurchaseRequisition/Controllers/UserController.cs
+++ PurchaseRequisition/PurchaseRequisition/Controllers/UserController.cs
@@ -9,6 +9,7 @@ namespace PurchaseRequisition.Controllers
     public class UserController : Controller
     {
         // GET: User
+        [Authorize(Roles = "User")]
         public ActionResult Index()
         {
             return View();
