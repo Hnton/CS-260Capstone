a6879e16e4005a1d056ea73da1d404eff95c7035
Mikael Hinton <43019867+Hnton@users.noreply.github.com>
2019-05-02 11:57:15 -0400

    Revert "Password Reset"
    This reverts commit 98aa74be6de66967db3b154a85c473eb2be57a0f.

--- PurchaseRequisition/PurchaseRequisition/Controllers/AccountController.cs
+++ PurchaseRequisition/PurchaseRequisition/Controllers/AccountController.cs
@@ -246,77 +246,39 @@ namespace PurchaseRequisition.Controllers
             return View();
         }
 
-        // GET: /AccountAdmin/ResetPassword
-        public ActionResult ResetPassword(string id)
+        //
+        // GET: /Account/ResetPassword
+        [AllowAnonymous]
+        public ActionResult ResetPassword(string code)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ResetPasswordViewModel model = new ResetPasswordViewModel() { Id = id };
-            return View(model);
+            return code == null ? View("Error") : View();
         }
 
         //
-        // POST: /AccountAdmin/ResetPassword
+        // POST: /Account/ResetPassword
         [HttpPost]
+        [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public ActionResult ResetPassword(ResetPasswordViewModel model)
+        public async Task<ActionResult> ResetPassword(ResetPasswordViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
-
-            var removePassword = UserManager.RemovePassword(model.Id);
-            if (removePassword.Succeeded)
+            var user = await UserManager.FindByNameAsync(model.Email);
+            if (user == null)
             {
-                //Removed Password Success
-                var AddPassword = UserManager.AddPassword(model.Id, model.NewPassword);
-                if (AddPassword.Succeeded)
-                {
-                    return View("PasswordResetConfirm");
-                }
+                // Don't reveal that the user does not exist
+                return RedirectToAction("ResetPasswordConfirmation", "Account");
             }
-
-            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var result = await UserManager.ResetPasswordAsync(user.Id, model.Code, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("ResetPasswordConfirmation", "Account");
+            }
+            AddErrors(result);
+            return View();
         }
-
-
-
-        ////
-        //// GET: /Account/ResetPassword
-        //[AllowAnonymous]
-        //public ActionResult ResetPassword(string code)
-        //{
-        //    return code == null ? View("Error") : View();
-        //}
-
-        ////
-        //// POST: /Account/ResetPassword
-        //[HttpPost]
-        //[AllowAnonymous]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> ResetPassword(ResetPasswordViewModel model)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return View(model);
-        //    }
-        //    var user = await UserManager.FindByNameAsync(model.Email);
-        //    if (user == null)
-        //    {
-        //        // Don't reveal that the user does not exist
-        //        return RedirectToAction("ResetPasswordConfirmation", "Account");
-        //    }
-        //    var result = await UserManager.ResetPasswordAsync(user.Id, model.Code, model.Password);
-        //    if (result.Succeeded)
-        //    {
-        //        return RedirectToAction("ResetPasswordConfirmation", "Account");
-        //    }
-        //    AddErrors(result);
-        //    return View();
-        //}
 
         //
         // GET: /Account/ResetPasswordConfirmation
